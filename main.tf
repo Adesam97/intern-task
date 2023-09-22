91d6bc0306dc4f1bf7a478eb3338390b95be5dd9
fikunmi samson <adejaresamson42@gmail.com>
2023-09-22 06:14:47 +0100

    added monitoring stack

--- main.tf
+++ main.tf
@@ -10,9 +10,31 @@ terraform {
 }
 
 provider "kubectl" {
-  config_path = "~/.kube/config" # Path to Kubernetes config file
+  config_path = "~/.kube/config" 
 }
 
-resource "kubectl_manifest" "example" {
-  yaml_body = file("kubernete.yml") # Path to Kubernetes manifest file
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"  
+  }
 }
+
+
+resource "kubectl_manifest" "example" {
+  yaml_body = file("kubernete.yml") 
+}
+
+resource "helm_release" "kube_prometheus" {
+  name       = "kube-prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "monitoring"
+  version    = "17.0.1" 
+
+  set {
+    name  = "prometheusOperator.createCustomResource"
+    value = "false"
+  }
+}
+
+
